16fd4f04b4b9029a3c3cd15a37efaa670d2b7117
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-04-05 01:02:16 +0530

    login containers

---
+++ lib/components/LoginBody.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:speedconnect/constants.dart';
+
+import 'LoginBodyBackground.dart';
+import 'RoundedEmailButton.dart';
+import 'RoundedPasswordButton.dart';
+
+class LoginBody extends StatelessWidget {
+  const LoginBody({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return LoginBodyBackground(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'LOGIN',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          SvgPicture.asset(
+            'assets/icons/login.svg',
+            height: size.height * 0.35,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          RoundedTextFeild(
+            onChanged: (value) {},
+            hint: 'Your Email',
+            icon: Icons.person,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          RoundedPasswordButton(
+            onChanged: (value) {},
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TextFeildContainer extends StatelessWidget {
+  final Widget child;
+  const TextFeildContainer({
+    Key key,
+    this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      width: size.width * 0.8,
+      decoration: BoxDecoration(
+          color: kPrimaryLightColor, borderRadius: BorderRadius.circular(29)),
+      child: child,
+    );
+  }
+}
