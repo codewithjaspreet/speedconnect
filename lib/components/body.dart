5599d8aa16614d8a93e3185b3bf754410c6e7d07
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-04-05 01:02:55 +0530

    updated

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:speedconnect/constants.dart';
 
+import '../Screens/Login.dart';
 import 'RoundedButton.dart';
 import 'background.dart';
 
@@ -11,25 +12,40 @@ class Body extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
 
     return Background(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'Welcome to Speed to Connect',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          SvgPicture.asset('assets/icons/chat.svg', height: size.height * 0.45),
-          RoundedButton(
-            color: kPrimaryColor,
-            text: 'LOGIN',
-            textColor: Colors.black,
-          ),
-          RoundedButton(
-            textColor: Colors.black,
-            color: kPrimaryColor,
-            text: 'SIGN UP',
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome to Speed Connect',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            SizedBox(
+              height: size.height * 0.05,
+            ),
+            SvgPicture.asset('assets/icons/chat.svg',
+                height: size.height * 0.45),
+            SizedBox(
+              height: size.height * 0.05,
+            ),
+            RoundedButton(
+              press: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return LoginScreen();
+                }));
+              },
+              color: kPrimaryColor,
+              text: 'LOGIN',
+              textColor: Colors.black,
+            ),
+            RoundedButton(
+              textColor: Colors.black,
+              color: kPrimaryLightColor,
+              press: () {},
+              text: 'SIGN UP',
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF6F35A5);
-const kPrimaryLightColor = Color(0xFFA1E6FF);
+const kPrimaryLightColor = Colors.grey;
